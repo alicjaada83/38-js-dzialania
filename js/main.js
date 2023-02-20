a9a08af3f3d8a03f7b6052b323136b6808b2c881
alicjaada83 <alicja.adamczyk83@gmail.com>
2023-02-20 19:47:41 +0100

    odpowiedz1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let num1 = 3;
+let num2 = 1;
+
+let suma = num1 + num2;
+if (suma < 0) {
+    console.log ('Wynik jest nieprawidłowy');
+} else {
+    console.log( ('Wynik dodawania wynosi'), (suma) );
+}
+
+let roznica = num1 - num2;
+if (roznica < 0) {
+    console.log ('Wynik jest nieprawidłowy');
+} else {
+    console.log( ('Wynik odejmowania wynosi'), (roznica) );
+}
+
+let iloczyn = num1 * num2;
+if (iloczyn < 0) {
+    console.log ('Wynik jest nieprawidłowy');
+} else {
+    console.log( ('Wynik mnożenia wynosi'), (iloczyn) );
+}
